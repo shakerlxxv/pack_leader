fae03003f184c0823bc10132e679bf363e250450
Brian Shaver <brian@4-dogs.biz>
2011-02-04 11:12:11 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PackLeader::Application.config.secret_token = '0fe80175190955a446fdd314b1e86bf6d7fa4e633983ed3d2d5f7a28670f2aef7558b13319b2971a0a073a7ba8ee43a735307eeaa5555b21d549ca87975eae6f'
