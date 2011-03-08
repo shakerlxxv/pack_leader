e42f6d92f3c4d90f5cf389ccd452eb84bcfc2f84
Brian Shaver <brian@4-dogs.biz>
2011-03-08 17:00:41 -0500

    Installed jQuery w/ custom theme

---
+++ public/javascripts/custom.js
@@ -0,0 +1,13 @@
+
+$(function() {
+        $( "#user_submit" ).button({
+            icons: {
+                       primary: "ui-icon-locked"
+                   }, text: true
+        });
+
+        $( "#test_button" ).button({
+            icons: { primary: "ui-icon-locked" },
+            text: true
+        });
+});
